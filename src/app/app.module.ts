a2724e00c364a9399d180a51e6c36c6488ddbf50
Fangmin Wei Lum <amberlum@fitiny.com>
2018-04-11 17:40:18 -0700

    Angular forms: formModule-template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,8 @@ import { ProductRowComponent } from './product-row/product-row.component';
 import { ProductImageComponent } from './product-image/product-image.component';
 import { PriceDisplayComponent } from './price-display/price-display.component';
 import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { FormsDemoComponent } from './forms-demo/forms-demo.component';
+import { FormWithBuilderComponent } from './form-with-builder/form-with-builder.component';
 
 
 @NgModule({
@@ -17,10 +20,14 @@ import { ProductDepartmentComponent } from './product-department/product-departm
     ProductRowComponent,
     ProductImageComponent,
     PriceDisplayComponent,
-    ProductDepartmentComponent
+    ProductDepartmentComponent,
+    FormsDemoComponent,
+    FormWithBuilderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-with-builder/form-with-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-with-builder',
+  templateUrl: './form-with-builder.component.html',
+  styleUrls: ['./form-with-builder.component.css']
+})
+export class FormWithBuilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
